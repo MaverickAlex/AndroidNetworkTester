2a51cfd618a9a6723962cd50170064400c284e86
MaverickAlex <maverick.alex@gmail.com>
2025-06-04 06:42:49 -0600

    Add item layout and update InfoAdapter to use it in RecyclerView

--- NetworkTester/InfoAdapter.cs
+++ NetworkTester/InfoAdapter.cs
@@ -16,11 +16,21 @@ public class InfoAdapter:RecyclerView.Adapter
   {
     var item = Items[position];
     var infoItem = (InfoItem)holder;
+
     infoItem.Name.Text = item;
     infoItem.Value.Text = item;
   }
-  public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType) =>
-    new InfoItem(parent);
+  public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+  {
+    //Setup your layout here
+    View itemView = null;
+    var id = Resource.Layout.item;
+    itemView = LayoutInflater.From(parent.Context).
+      Inflate(id, parent, false);
+
+    var vh = new InfoItem(itemView);
+    return vh;
+  }
 
   public override int ItemCount => Items.Length;
 }
